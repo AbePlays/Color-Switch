b166f73080f4d30cc079a8fc7f9168789c531f2c
Abe <abhi.rawat456@gmail.com>
2020-03-07 15:56:39 +0530

    added animations to the label

--- Color Switch/Scenes/MenuScene.swift
+++ Color Switch/Scenes/MenuScene.swift
@@ -30,6 +30,7 @@ class MenuScene: SKScene {
         playLabel.fontColor = .white
         playLabel.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(playLabel)
+        animate(label: playLabel)
         
         let highScoreLabel = SKLabelNode(text: "High Score : \(UserDefaults.standard.integer(forKey: "HighScore"))")
         highScoreLabel.fontName = "AvenirNext-Bold"
@@ -44,6 +45,16 @@ class MenuScene: SKScene {
         recentScoreLabel.fontColor = .white
         recentScoreLabel.position = CGPoint(x: frame.midX, y: highScoreLabel.position.y - recentScoreLabel.frame.size.height*2)
         addChild(recentScoreLabel)
+    }
+    
+    func animate(label : SKLabelNode) {
+//        let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+//        let fadeIn = SKAction.fadeIn(withDuration: 0.5)
+        
+        let scaleUp = SKAction.scale(to: 1.1, duration: 0.5)
+        let scaleDown = SKAction.scale(to: 1.0, duration: 0.5)
+        let sequence = SKAction.sequence([scaleUp, scaleDown])
+        label.run(SKAction.repeatForever(sequence))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
